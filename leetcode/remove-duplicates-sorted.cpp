ab99e7891cd60002fa4f4aab8fbf69387772eb93
Keane <keanemind@gmail.com>
2018-09-21 15:30:53 -0500

    Correct solution

--- leetcode/remove-duplicates-sorted.cpp
+++ leetcode/remove-duplicates-sorted.cpp
@@ -3,20 +3,23 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        if (nums.empty()) {
-            return 0;
+        if (nums.size() < 2) {
+            return nums.size();
         }
 
         int i = 1;
         int j = 1;
         int last;
+        if (nums[1] != nums[0]) {
+            i = 2;
+        }
         while (true) {
             last = nums[j];
             while (j < nums.size() && nums[j] == last) {
                 j++;
             }
             if (j == nums.size()) {
-                return i+1;
+                return i; // i starts at 1
             }
             // j now points to next number
             nums[i] = nums[j];
